39b0877368f5731bed029eaf081be8320dfb6b28
Bruce M. Klink <b-klink@kellogg.northwestern.edu>
2013-11-13 00:04:57 -0600

    picking up part 2 of class

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -15,6 +15,25 @@ class PicturesController < ApplicationController
     p.source = params[:source]
     p.caption = params[:caption]
     p.save
-    redirect_to "http://localhost:3000/all_pictures"
+    #write code for it to be shared to facebook and twitter for example "Twitter::Client.tweet("this is my tweeeet! :)")" or "Twitter::Client.tweet( <%= params[:source] %>)"
+    redirect_to "http://localhost:3000/all_pictures", :notice => "Picture posted successfully!"
+  end
+
+  def destroy
+    p = Picture.find(params[:id])
+    p.destroy
+    redirect_to "http://localhost:3000/all_pictures", :notice => "Picture exterminated."
+  end
+
+  def edit
+    @picture = Picture.find(params[:id])
+  end
+
+  def update
+    p = Picture.find(params[:id])
+    p.source = params[:source]
+    p.caption = params[:caption]
+    p.save  
+    redirect_to "http://localhost:3000/picture_details/#{p.id}", :notice => "Picture updated! :)"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rxngif::Application.routes.draw do
   get("/new_picture_form", { :controller => "pictures", :action => "new" })
   get("/all_pictures", { :controller => "pictures", :action => "index" })
   get("/picture_details/:id", { :controller => "pictures", :action => "show" })
+  get("/delete_picture/:id", { :controller => "pictures", :action => "destroy" })
+  get("/edit_picture_form/:id", { :controller => "pictures", :action => "edit" })
+  get("/update_picture/:id", { :controller => "pictures", :action => "update" })
 end
