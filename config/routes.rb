676503c3a386998784358ebbdfc183b93dd89448
Bruce M. Klink <b-klink@kellogg.northwestern.edu>
2013-11-13 07:53:12 -0600

    Redirects, View Helper Methods, and RESTful Routes complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,5 @@
 Rxngif::Application.routes.draw do
-  #post("/picture", :controller => "pictures", :action => "create" , 
-  #get("/new_picture_form", { :controller => "pictures", :action => "new", :as => "new_picture" })
-  #get("/all_pictures", { :controller => "pictures", :action => "index" })
-  #get("/picture_details/:id", { :controller => "pictures", :action => "show" })
-  #get("/edit_picture_form/:id", { :controller => "pictures", :action => "edit" })
-  #patch("/update_picture/:id", { :controller => "pictures", :action => "update" })
-  #delete("/delete_picture/:id", { :controller => "pictures", :action => "destroy" })
-
- 	 # CREATE
+    # CREATE
 	get '/posts/new', :controller => 'posts', :action => 'new', :as => 'new_picture'
 	post '/posts', :controller => 'posts', :action => 'create', :as => 'pictures'
 
